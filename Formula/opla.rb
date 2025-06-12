a87bb24bed00f42e506d9e09f6d25dc4e635337e
Matthieu via GitLab CI <matthieu.boileau@math.unistra.fr>
2025-06-12 09:44:10 +0000

    Update opla to v1.1.3

--- Formula/opla.rb
+++ Formula/opla.rb
@@ -3,9 +3,9 @@ class Opla < Formula
 
   desc "A personal webpage generator"
   homepage "https://gitlab.math.unistra.fr/irma/opla"
-  url "https://gitlab.math.unistra.fr/irma/opla/-/archive/v1.1.2/opla-v1.1.2.tar.gz"
-  version "v1.1.2"
-  sha256 "9089bf9a3269acedff4d0caa1ce4e378582d5ecae0590009dcb6ab308affb0c2"
+  url "https://gitlab.math.unistra.fr/irma/opla/-/archive/v1.1.3/opla-v1.1.3.tar.gz"
+  version "v1.1.3"
+  sha256 "8dac9680d9f9466d64bb468ccace61afe0048a6daf4e339233136a8dbf744613"
   license "MIT"
 
   depends_on "python@3.11"
