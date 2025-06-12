27bcd60fa9fc00fe2381eb2ed1c265ca79c66743
Matthieu via GitLab CI <matthieu.boileau@math.unistra.fr>
2025-06-12 09:33:21 +0000

    Update opla to v1.1.2

--- Formula/opla.rb
+++ Formula/opla.rb
@@ -3,25 +3,32 @@ class Opla < Formula
 
   desc "A personal webpage generator"
   homepage "https://gitlab.math.unistra.fr/irma/opla"
-  url "https://gitlab.math.unistra.fr/irma/opla/-/archive/v1.1.0/opla-v1.1.0.tar.gz"
-  sha256 "f412a0f1f2af19e9a448f74e4b711a3b1b49e289749cff64aa8a23cdcfe8c654"
+  url "https://gitlab.math.unistra.fr/irma/opla/-/archive/v1.1.2/opla-v1.1.2.tar.gz"
+  version "v1.1.2"
+  sha256 "9089bf9a3269acedff4d0caa1ce4e378582d5ecae0590009dcb6ab308affb0c2"
   license "MIT"
 
   depends_on "python@3.11"
 
   def install
-    # Create virtual environment with pip
     virtualenv_create(libexec, "python3.11", without_pip: false)
-    
-    # Install the package directly (let pip handle dependencies)
-    system libexec/"bin/pip", "install", "--upgrade", "pip"
     system libexec/"bin/pip", "install", buildpath
-    
-    # Create symlink to the executable
     bin.install_symlink libexec/"bin/opla"
   end
 
   test do
     system "#{bin}/opla", "--help"
+
+    (testpath/"test.md").write <<~EOS
+      ---
+      title: Test
+      ---
+      # Test
+
+      Ceci est un test.
+    EOS
+
+    system "#{bin}/opla", "test.md", "-o", "build"
+    assert_predicate testpath/"build/index.html", :exist?
   end
 end
